82ee9ed48a319551491648acace9b87efdfebde7
engedmundo <engedmundo@gmail.com>
2022-03-16 16:58:41 -0300

    definindo permissões via token na view attractions

--- apps/attractions/api/viewsets.py
+++ apps/attractions/api/viewsets.py
@@ -1,4 +1,5 @@
 from apps.attractions.models import Attraction
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.viewsets import ModelViewSet
 
 from .serializers import AttractionSerializer
@@ -7,6 +8,7 @@ from .serializers import AttractionSerializer
 class AttractionViewSet(ModelViewSet):
     #queryset = Attraction.objects.all()
     serializer_class = AttractionSerializer
+    authentication_classes = (TokenAuthentication,)
 
     def get_queryset(self):
         return Attraction.objects.filter(approved=True)
