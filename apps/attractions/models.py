b7608d983e59c39308f0e7ed20d119af6934fd90
engedmundo <engedmundo@gmail.com>
2022-03-16 11:52:29 -0300

    model benefits

--- apps/attractions/models.py
+++ apps/attractions/models.py
@@ -1,11 +1,24 @@
 from django.db import models
 
+from apps.benefits.models import Benefit
 
-# Create your models here.
+
 class Attraction(models.Model):
-    name = models.CharField(max_length=150)
-    descrption = models.TextField()
-    approved = models.BooleanField(default=False)
+    name = models.CharField(max_length=150, verbose_name="Nome")
+    descrption = models.TextField(verbose_name="Descrição")
+    approved = models.BooleanField(default=False, verbose_name="Aprovado")
+    benefit = models.ManyToManyField(Benefit, verbose_name="Recursos")
 
-    def __str__(self):
-        return self.name
+
+"""
+    comentarios = models.ManyToManyField(Comentario)
+    avaliacoes = models.ManyToManyField(Avaliacao)
+    endereco = models.ForeignKey(
+        Endereco, on_delete=models.SET_NULL, null=True)
+    foto = models.ImageField(
+        upload_to='pontos-turisticos', null=True, blank=True)
+"""
+
+
+def __str__(self):
+    return self.name
--- apps/benefits/models.py
+++ apps/benefits/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Benefit(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Nome")
+    description = models.TextField(verbose_name="Descrição")
+    opening_hours = models.TextField(verbose_name="Horário de funcionamento")
+    minimum_age = models.IntegerField(verbose_name="Idade mínima")
+
+    def __str__(self):
+        return self.name
