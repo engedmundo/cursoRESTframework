a7dc79afcfb477de3aec385f5484737f0b41c7fd
engedmundo <engedmundo@gmail.com>
2022-03-16 11:37:55 -0300

    registro apps settings. alteração arquivos apps

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'apps.attractions',
+    'apps.benefits',
+    'apps.comments',
+    'apps.locations',
+    'apps.ratings',
 ]
 
 MIDDLEWARE = [
